34c692ced4ce818068e688a8e89a7b6a755bfc8f
malevarro <61161340+malevarro@users.noreply.github.com>
2021-03-25 07:56:10 -0500

    Add files via upload

---
+++ entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+echo "[+] Setting environment variables"
+export SHOWSTOPPER_PRIORITY="HIGH,CRITICAL"
+export TRIVYCACHE="/.trivy_cache"
+export DOCKLERCACHE="/.dockler_cache"
+export ARTIFACT_FOLDER="json"
+export TRIVY_USERNAME=${USERNAME}
+export TRIVY_PASSWORD=${PASSWORD}
+export DOCKLE_USERNAME=${USERNAME}
+export DOCKLE_PASSWORD=${PASSWORD}
+
+cd /docker_tools
+mkdir $ARTIFACT_FOLDER
+
+# Hadolint
+echo "[+] Running Hadolint"
+echo "[+] Writing Hadolint JSON File" 
+echo "[+] Showing Hadolint Results" 
+./hadolint-Linux-x86_64 -f json /input_files/${DOCKERFILE} > $ARTIFACT_FOLDER/hadolint_results.json
+
+# show results
+./hadolint-Linux-x86_64 /input_files/${DOCKERFILE}
+
+# Dockle
+echo "[+] Running Dockle"
+# writing finding into files
+echo "[+] Writing Dockle JSON File" 
+dockle --cache-dir $DOCKLERCACHE --exit-code 1 -f json --output $ARTIFACT_FOLDER/dockle_results.json $DOCKERIMAGE
+echo "[+] Writing Dockle Text File" 
+dockle --cache-dir $DOCKLERCACHE --exit-code 1 $DOCKERIMAGE > /results/dockle_results.txt
+
+# Trivy
+echo "[+] Running Trivy"
+#Updating Vuln Database
+trivy image --clear-cache
+trivy image --download-db-only
+
+# writing finding into files
+echo "[+] Writing Trivy JSON File" 
+trivy --cache-dir $TRIVYCACHE -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 --quiet $DOCKERIMAGE
+echo "[+] Writing Trivy Text File" 
+trivy --cache-dir $TRIVYCACHE -f table -o /results/trivy_results.txt --exit-code 0 --quiet $DOCKERIMAGE
+
+# fail build if there is at least 1 vulnerability of the defined severity
+#echo "[+] Trivy High and Critical Vulnerabilities"
+#trivy -d --cache-dir $TRIVYCACHE --exit-code 1 --severity $SHOWSTOPPER_PRIORITY --quiet $DOCKERIMAGE
+
+# HTML results from all tools outputs
+echo "[+] Making the output look pretty"
+python3 ./convert_json_results.py
+mv $ARTIFACT_FOLDER/*.json /results
+mv results.html /results
+
+# Collect the results in docker_tools/results.html
+echo "[+] Everything is done. Find the resulting HTML report in results.html"
---
+++ tools_install_rh.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+export TRIVYCACHE="/.trivy_cache"
+export ARTIFACT_FOLDER="./json"
+
+# installing all necessary stuff
+echo "[+] Installing required packages"
+#apt-get update
+yum update
+#apt-get install -y wget git python3 python3-pip unzip rpm libopenscap8 ssg-debian ssg-debderived ssg-nondebian ssg-applications
+yum install -y wget git python3 python3-pip unzip
+
+# preparing directory structure
+echo "[+] Preparing necessary directories"
+mkdir /docker_tools
+cd /docker_tools
+mkdir $TRIVYCACHE
+mkdir $ARTIFACT_FOLDER
+
+# Trivy
+echo "[+] Fetching Trivy"
+export TRIVYVERSION=$(git ls-remote --refs --sort="version:refname" --tags https://github.com/aquasecurity/trivy | cut -d/ -f3-|tail -n1 | sed -e 's/^.//')
+echo "[+] Trivy Version:" ${TRIVYVERSION}
+#wget -nv --no-cache https://github.com/aquasecurity/trivy/releases/download/v${TRIVYVERSION}/trivy_${TRIVYVERSION}_Linux-64bit.deb
+wget -nv --no-cache https://github.com/aquasecurity/trivy/releases/download/v${TRIVYVERSION}/trivy_${TRIVYVERSION}_Linux-64bit.rpm
+echo "[+] Installing Trivy"
+#dpkg -i trivy_${TRIVYVERSION}_Linux-64bit.deb
+rpm -i trivy_${TRIVYVERSION}_Linux-64bit.rpm
+echo "[+] Trivy Installed "${TRIVYVERSION}
+
+# Dockle
+echo "[+] Fetching Dockle"
+export DOCKLEVERSION=$(git ls-remote --refs --sort="version:refname" --tags https://github.com/goodwithtech/dockle | cut -d/ -f3-|tail -n1 | sed -e 's/^.//')
+#wget -nv --no-cache https://github.com/goodwithtech/dockle/releases/download/v${DOCKLEVERSION}/dockle_${DOCKLEVERSION}_Linux-64bit.deb
+wget -nv --no-cache https://github.com/goodwithtech/dockle/releases/download/v${DOCKLEVERSION}/dockle_${DOCKLEVERSION}_Linux-64bit.rpm
+#dpkg -i dockle_${DOCKLEVERSION}_Linux-64bit.deb
+rpm -i dockle_${DOCKLEVERSION}_Linux-64bit.rpm
+echo "Dockle Installed "$DOCKLEVERSION
+
+# Hadolint
+echo "[+] Fetching Hadolint"
+export HADOLINTVERSION=$(git ls-remote --refs --sort="version:refname" --tags https://github.com/hadolint/hadolint | cut -d/ -f3-|tail -n1 | sed -e 's/^.//')
+echo "[+] Hadolint Version:" ${HADOLINTVERSION}
+wget -nv --no-cache https://github.com/hadolint/hadolint/releases/download/v${HADOLINTVERSION}/hadolint-Linux-x86_64 && chmod +x hadolint-Linux-x86_64
+
+# cleaning up
+echo "[+] Removing left-overs"
+#rm *.deb #LICENSE README.md 
+rm *.rpm #LICENSE README.md 
+
+# json converting script
+echo "[+] Fetching json2HTML"
+pip3 install json2html
+wget -nv --no-cache -O convert_json_results.py https://raw.githubusercontent.com/Swordfish-Security/docker_cicd/master/convert_json_results.py
+
+echo "[+] Image has been built"
