fa61b13e07f226ed861611b908c7aff8b619b797
malevarro <alejo.vargas@gmail.com>
2023-02-08 11:05:13 -0500

    trivy quiet

--- entrypoint.sh
+++ entrypoint.sh
@@ -39,22 +39,22 @@ dockle --cache-dir $DOCKLERCACHE --exit-code 1 $DOCKERIMAGE > /results/dockle_re
 # Trivy
 echo "[+] Running Trivy"
 #clean Vuln Database
-trivy --cache-dir $TRIVYCACHE image --clear-cache
-trivy --cache-dir $TRIVYCACHE image --download-db-only
+trivy --quiet --cache-dir $TRIVYCACHE image --clear-cache
+trivy --quiet --cache-dir $TRIVYCACHE image --download-db-only
 # writing finding into files
 echo "***Vulneability Assesment***"
 echo "[+] Writing Trivy JSON File" 
-trivy --cache-dir $TRIVYCACHE image --skip-db-update -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 $DOCKERIMAGE
+trivy --quiet --cache-dir $TRIVYCACHE image --skip-db-update -f json -o $ARTIFACT_FOLDER/trivy_results.json --exit-code 0 $DOCKERIMAGE
 echo "[+] Writing Trivy Text File" 
-trivy --cache-dir $TRIVYCACHE image --skip-db-update -f table --exit-code 0 $DOCKERIMAGE > /results/trivy_results.txt
+trivy --quiet --cache-dir $TRIVYCACHE image --skip-db-update -f table --exit-code 0 $DOCKERIMAGE > /results/trivy_results.txt
 echo "***SBOM Analysis***"
 echo "[+] Writing Trivy SBOM JSON File" 
-trivy --cache-dir $TRIVYCACHE image --scanners vuln --format cyclonedx --output $ARTIFACT_FOLDER/trivy_sbom_results.json --exit-code 0 $DOCKERIMAGE
+trivy --quiet --cache-dir $TRIVYCACHE image --scanners vuln --format cyclonedx --output $ARTIFACT_FOLDER/trivy_sbom_results.json --exit-code 0 $DOCKERIMAGE
 echo "***Docker Compliance Analysis***"
 echo "[+] Writing Trivy Docker Compliance File" 
-trivy --cache-dir $TRIVYCACHE image --compliance docker-cis $DOCKERIMAGE > /results/trivy_compliance_results.txt
+trivy --quiet --cache-dir $TRIVYCACHE image --compliance docker-cis $DOCKERIMAGE > /results/trivy_compliance_results.txt
 echo "[+] Writing Trivy Docker Compliance JSON File" 
-trivy --cache-dir $TRIVYCACHE image --compliance docker-cis --format json --output $ARTIFACT_FOLDER/trivy_compliance_results.json $DOCKERIMAGE
+trivy --quiet --cache-dir $TRIVYCACHE image --compliance docker-cis --format json --output $ARTIFACT_FOLDER/trivy_compliance_results.json $DOCKERIMAGE
 
 # fail build if there is at least 1 vulnerability of the defined severity
 #echo "[+] Trivy High and Critical Vulnerabilities"
